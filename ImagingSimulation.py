ebe42b40e5a9e6c0c2b80fce459307fabbdb28ed
SalvatoreLag <salvatore.lagrasta01@gmail.com>
2024-10-09 16:21:06 +0200

    Imaging simulation completed

--- ImagingSimulation.py
+++ ImagingSimulation.py
@@ -9,9 +9,12 @@ N = 5
 diameter = 0.9
 
 p = ap.tpa_positionsV2(N,diameter)
+fig,ax = plt.subplots()
+ax.scatter(p[:,0],p[:,1])
+plt.savefig('./Outputs/positions.png')
 
 # Clean image
-filename = './CleanImages/LowRes.txt'
+filename = './CleanImages/LowRes2.txt'
 clean_img = np.loadtxt(filename)
 
 nside = hp.npix2nside(len(clean_img))
@@ -22,10 +25,10 @@ u,v,_ = hp.pix2vec(nside,sky_pixels)
 
 fig,ax = plt.subplots()
 ax.tricontourf(u,v,clean_img[sky_pixels])
-plt.show()
+plt.savefig('./Outputs/clean_uv.png')
 
 hp.orthview(clean_img,rot=[-90,90,0])
-plt.show()
+plt.savefig('./Outputs/clean_orth.png')
 
 # Element pattern
 filename = './HealpixPatterns/Farfield120_5GHz.txt'
@@ -35,9 +38,29 @@ E_plot = 20*np.log10(E/np.max(E))
 
 fig,ax = plt.subplots()
 ax.tricontourf(u,v,E_plot[sky_pixels])
-plt.show()
+plt.savefig('./Outputs/pattern_uv.png')
 
 hp.orthview(E,rot=[0,90,0])
-plt.show()
+plt.savefig('./Outputs/pattern_orth.png')
+
+# Array pattern and imaging
+# scan_fov = 30
+# scan_pixels = hp.query_disc(nside,sky_center,scan_fov)
+# A = ab.array_pattern(nside,sky_pixels,scan_pixels,p)
+# dirty_img = A@clean_img[sky_pixels]
+
+dirty_img = np.ones_like(clean_img)*np.inf
+for pix in sky_pixels:
+    pix = np.atleast_1d(pix)
+    A = ab.array_pattern(nside,sky_pixels,pix,p)
+    dirty_img[pix] = A@clean_img[sky_pixels]
+
+fig,ax = plt.subplots()
+ax.tricontourf(u,v,dirty_img[sky_pixels])
+plt.savefig('./Outputs/dirty_uv.png')
+
+hp.orthview(dirty_img,rot=[0,90,0])
+plt.savefig('./Outputs/dirty_orth.png')
+
 
 
--- array_beam.py
+++ array_beam.py
@@ -7,10 +7,8 @@ def array_pattern(nside,source_pixels,scan_pixels,positions):
     s0x,s0y,_ = hp.pix2vec(nside,scan_pixels)
     S0 = np.stack((s0x,s0y))
 
-    nsource = source_pixels.size
-    nscan = scan_pixels.size
-    if nscan == 1:
-        S0 = np.expand_dims(S0,1)
+    nsource = len(source_pixels)
+    nscan = len(scan_pixels)
     S = np.tile(S,(1,nscan))
     S0 = np.repeat(S0,nsource,axis=1)
 
