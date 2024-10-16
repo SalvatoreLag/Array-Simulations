7af0e39ae70aea5ec61bb05910ee2e8a0b51b5d8
SalvatoreLag <salvatore.lagrasta01@gmail.com>
2024-10-16 13:28:58 +0200

    Plots and documentation

--- BeamPlots.py
+++ BeamPlots.py
@@ -37,6 +37,39 @@ ax.set_ylabel('v [-]')
 plt.colorbar(im,ax=ax)
 plt.savefig('./Outputs/ElementPattern.png')
 
+# Array pattern - Theta-phi grid approach
+# Define steering angle
+theta0 = np.radians(90)
+phi0 = np.radians(0)
+
+# Compute steered array pattern 
+A_grid = af.array_pattern_grid(theta,phi,theta0,phi0,p)
+
+A_plot = 10*np.log10(A_grid/np.max(A_grid))
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot()
+im = ax.pcolor(U,V,A_plot,vmin=-40)
+ax.axis('equal')
+ax.set(xlim=(-1,1),ylim=(-1,1))
+ax.set_xlabel('u [-]')
+ax.set_ylabel('v [-]')
+plt.colorbar(im,ax=ax)
+plt.savefig('./Outputs/AsteeredGrid.png')
+
+# With real elements
+A_elem = (E**2)*A_grid
+
+A_plot = 10*np.log10(A_elem/np.max(A_elem))
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot()
+im = ax.pcolor(U,V,A_plot,vmin=-40)
+ax.axis('equal')
+ax.set(xlim=(-1,1),ylim=(-1,1))
+ax.set_xlabel('u [-]')
+ax.set_ylabel('v [-]')
+plt.colorbar(im,ax=ax)
+plt.savefig('./Outputs/AElementGrid.png')
+
 # Array pattern - Healpix approach
 # Define visible space
 nside = 64
@@ -47,16 +80,16 @@ u,v,_ = hp.pix2vec(nside,sky_pixels)
 
 # Define scanning space
 scan_fov = 30
-scan_pixels = np.atleast_1d(6000) #np.atleast_1d(hp.query_disc(nside,sky_center,scan_fov))
+scan_pixels = np.atleast_1d(hp.ang2pix(nside,theta0,phi0)) #np.atleast_1d(hp.query_disc(nside,sky_center,scan_fov))
 
 # Get matrix of steered beams
 A_matrix = af.array_pattern_matrix(nside,sky_pixels,scan_pixels,p)
 
 # Plot normalized steered beam
-A_steered = 10*np.log10(A_matrix[0,:])-10*np.log10(max(A_matrix[0,:]))
+A_plot = 10*np.log10(A_matrix[0,:])-10*np.log10(max(A_matrix[0,:]))
 fig = plt.figure(figsize=(8,6))
 ax = fig.add_subplot()
-im = ax.tripcolor(u,v,A_steered,vmin=-40)
+im = ax.tripcolor(u,v,A_plot,vmin=-40)
 ax.axis('equal')
 ax.set(xlim=(-1,1),ylim=(-1,1))
 ax.set_xlabel('u [-]')
@@ -64,21 +97,19 @@ ax.set_ylabel('v [-]')
 plt.colorbar(im,ax=ax)
 plt.savefig('./Outputs/AsteeredHP.png')
 
-# Array pattern - Theta-phi grid approach
-# Define steering angle
-theta0,phi0 = hp.pix2ang(nside,scan_pixels)
+# With real elements
+filename = './HealpixPatterns/Farfield120_5GHz.txt'
+E = np.loadtxt(filename)
+E = E[sky_pixels]
+A_elem = (E**2)*A_matrix[0,:]
 
-# Compute steered array pattern 
-A_grid = af.array_pattern_grid(theta,phi,theta0,phi0,p)
-A_grid = 10*np.log10(A_grid/np.max(A_grid))
-
+A_plot = 10*np.log10(A_elem/np.max(A_elem))
 fig = plt.figure(figsize=(8,6))
 ax = fig.add_subplot()
-im = ax.pcolor(U,V,A_grid,vmin=-40)
+im = ax.tripcolor(u,v,A_plot,vmin=-40)
 ax.axis('equal')
 ax.set(xlim=(-1,1),ylim=(-1,1))
 ax.set_xlabel('u [-]')
 ax.set_ylabel('v [-]')
 plt.colorbar(im,ax=ax)
-plt.savefig('./Outputs/AsteeredGrid.png')
-
+plt.savefig('./Outputs/AElementHP.png')
